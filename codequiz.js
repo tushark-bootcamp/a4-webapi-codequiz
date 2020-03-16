84e6bd9d44d653c8a7cc9a66545df71135075b78
TusharK <ktusharhk@gmail.com>
2020-03-16 20:08:08 +1100

    pushing interim version

--- codequiz.js
+++ codequiz.js
@@ -1,9 +1,8 @@
-var quizNumber = 0;
-var answerOptionsUL = document.querySelector("#answer-options");
+var quizQuestionUL = document.querySelector("#quiz-question");
 var codeQuizForm = document.querySelector("#code-quiz-form");
 var resultPara = document.querySelector("#result");
 var resultDiv = document.querySelector("#resultDiv");
-
+//var lastQuestionAnswered
 
 var score = 0;
 //var startQuizButton = null;
@@ -11,11 +10,10 @@ var score = 0;
 var stateMachine = {
     quizInitiated: false,
     displayingScore: false,
-    quizIndex: 0,
-    score: 0
+    quizIndex: 0
 }
 
-const quizQuestionBank = [
+var quizQuestionBank = [
     {
         quizIndex: 0,
         question: "Who invented JavaScript?",
@@ -25,7 +23,8 @@ const quizQuestionBank = [
             "c: Brendan Eich"
         ],
         correctAnswer: "c",
-        correctIndex: 2
+        correctIndex: 2,
+        score: 0
     },
     {
         quizIndex: 1,
@@ -36,7 +35,8 @@ const quizQuestionBank = [
             "c: npm"
         ],
         correctAnswer: "c",
-        correctIndex: 2
+        correctIndex: 2,
+        score: 0
     },
     {
         quizIndex: 2,
@@ -48,7 +48,8 @@ const quizQuestionBank = [
             "d: ESLint"
         ],
         correctAnswer: "d",
-        correctIndex: 3
+        correctIndex: 3,
+        score: 0
     }
 ];
 
@@ -63,54 +64,179 @@ function initiateQuiz() {
     startQuizButton.classList.add("btn", "btn-secondary", "btn-sm");
     startQuizButton.textContent = "Start Quiz";
 
-    startQuizButton.setAttribute("quiz-index", 0);
+    //startQuizButton.setAttribute("quiz-index", 0);
+    startQuizButton.setAttribute("id", "start-quiz");
     li.appendChild(startQuizButton);
-    answerOptionsUL.appendChild(li);
+    quizQuestionUL.appendChild(li);
     stateMachine.quizInitiated = true;
 }
 
-
-
-answerOptionsUL.addEventListener("click", function (event) {
-    //alert("answerOptionsUL.click");
+quizQuestionUL.addEventListener("click", function (event) {
+    //alert("quizQuestionUL.click");
     var eventSource = event.target;
     if (eventSource.matches("button")) {
+        //alert("stateMachine.quizIndex: " + stateMachine.quizIndex);
+        if (eventSource.getAttribute("id") == "start-quiz") {
+            renderNextQuiz(0);
+        } else if (eventSource.getAttribute("id") == "quiz-question") {
+            var quizInd = eventSource.getAttribute("quiz-index");
+            var userAnswerInd = eventSource.getAttribute("answer-index");
+            if (quizInd < quizQuestionBank.length - 1) {
+                var answeredCorrectly = false;
+                if (userAnswerInd == quizQuestionBank[quizBankInd].correctIndex) {
+                    answeredCorrectly = true;
+                }
+                displayAnswer(answeredCorrectly, quizBankInd);
+                if ((stateMachine.quizIndex < quizQuestionBank.length)) {
+                    setTimeout(renderNextQuiz, 1500);
+                }
+
+            }
+        }
         if (stateMachine.quizIndex == 0) {
             //alert("stateMachine.quizIndex: " + stateMachine.quizIndex);
-            renderNextQuiz(stateMachine.quizIndex);
-            stateMachine.quizIndex++;
-        } else {
+            renderNextQuiz();
+        } else if (stateMachine.quizIndex <= quizQuestionBank.length) {
             //alert("stateMachine.quizIndex: " + stateMachine.quizIndex);
             // When stateMachine.quizIndex > 0, the event source is the answer button chosen by the user
-            var answerInd = eventSource.getAttribute("answer-index");
+            var userAnswerInd = eventSource.getAttribute("answer-index");
             //alert("answerInd: " + answerInd);
             //alert("quizQuestionBank[stateMachine.quizIndex].correctIndex: " + quizQuestionBank[stateMachine.quizIndex].correctIndex);
-            if (answerInd == quizQuestionBank[stateMachine.quizIndex].correctIndex) {
-                displayAnswer(true);
+            var quizBankInd = 0;
+            if (stateMachine.quizIndex == quizQuestionBank.length) {
+                quizBankInd = quizQuestionBank.length - 1;
             } else {
-                displayAnswer(false);
+                quizBankInd = stateMachine.quizIndex;
             }
-            // pause() var quizNo = eventSource.getAttribute("quiz-index");
-            pause(400);
-            renderNextQuiz(stateMachine.quizIndex);
-            stateMachine.quizIndex++;
+            var answeredCorrectly = false;
+            if (userAnswerInd == quizQuestionBank[quizBankInd].correctIndex) {
+                answeredCorrectly = true;
+            }
+            displayAnswer(answeredCorrectly, quizBankInd);
+            if ((stateMachine.quizIndex < quizQuestionBank.length)) {
+                setTimeout(renderNextQuiz, 1500);
+            }
+        } else {
+            alert("All questions answered");
         }
     }
 });
 
-function displayAnswer(result) {
+function renderNextQuiz(quizIndex) {
+    //var index = stateMachine.quizIndex;
+    //alert("stateMachine.quizIndex: " + stateMachine.quizIndex);
+    resultDiv.style.display = "none";
+    var quiz = quizQuestionBank[quizIndex];
+    var question = quiz.question;
+    //alert("question: " + question);
+    var answerOptions = quiz.answers;
+    quizQuestionUL.textContent = question;
+    quizQuestionUL.setAttribute("quiz-index", quizIndex);
+    for (var i = 0; i < answerOptions.length; i++) {
+        var answerI = answerOptions[i];
+        //alert("answerI: " + answerI);
+        var liOptions = document.createElement("li");
+        var answerButton = document.createElement("button");
+        answerButton.classList.add("btn", "btn-secondary", "btn-sm");
+        answerButton.textContent = answerI;
+
+        answerButton.setAttribute("answer-index", i);
+        answerButton.setAttribute("quiz-index", quizIndex);
+
+        liOptions.appendChild(answerButton);
+        quizQuestionUL.appendChild(liOptions);
+    }
+    // Only increment quizIndex until it reaches the length - 1 of the Array of quizbank
+    if (stateMachine.quizIndex < quizQuestionBank.length) {
+        stateMachine.quizIndex++;
+    }
+}
+
+function renderNextQuiz(quizInd) {
+    //var index = stateMachine.quizIndex;
+    //alert("stateMachine.quizIndex: " + stateMachine.quizIndex);
+    resultDiv.style.display = "none";
+    var quiz = quizQuestionBank[stateMachine.quizIndex];
+    var question = quiz.question;
+    //alert("question: " + question);
+    var answerOptions = quiz.answers;
+    quizQuestionUL.textContent = question;
+    quizQuestionUL.setAttribute("quiz-index", quiz.quizIndex);
+    for (var i = 0; i < answerOptions.length; i++) {
+        var answerI = answerOptions[i];
+        //alert("answerI: " + answerI);
+        var liOptions = document.createElement("li");
+        var answerButton = document.createElement("button");
+        answerButton.classList.add("btn", "btn-secondary", "btn-sm");
+        answerButton.textContent = answerI;
+
+        answerButton.setAttribute("answer-index", i);
+        //answerButton.setAttribute("quiz-index", index);
+
+        liOptions.appendChild(answerButton);
+        quizQuestionUL.appendChild(liOptions);
+    }
+    // Only increment quizIndex until it reaches the length - 1 of the Array of quizbank
+    if (stateMachine.quizIndex < quizQuestionBank.length) {
+        stateMachine.quizIndex++;
+    }
+}
+
+
+
+
+function displayAnswer(result, quizBankInd) {
     resultDiv.style.display = "block";
     var answer = "Correct!!";
     if (!result) {
         answer = "Wrong!!";
+    } else {
+        quizQuestionBank[quizBankInd].score = 1;
     }
-    //var hrElem = document.createElement("hr");
-    //hrElem.classList.add("bold-gray");
-    //resultDiv.prepend(hrElem);
-    //alert("displayAnswer + answer: " + answer);
-
     resultPara.textContent = answer;
 }
+
+function displayResult() {
+
+}
+
+
+
+/*function startQuiz(quizIndex) {
+    var eventSource = event.target;
+    if (eventSource.matches("button")) {
+        var quizNo = eventSource.getAttribute("quiz-index");
+        alert("quizNo: " + quizNo);
+        renderQuiz(quizIndex);
+    }
+}*/
+
+/*renderQuizAnswer(answer) {
+    var li = document.createElement("li");
+    li.textContent = answer;
+    quizQuestion.appendChild(li);
+}*/
+
+/* We will listen to the click event on the entire answerOptions<UL> object instead.
+codeQuizForm.addEventListener("submit", function (event) {
+    //event.preventDefault();
+    alert("codeQuizForm form submit");
+    takeQuiz(event);
+});
+
+
+quizQuestionUL.addEventListener("click", function (event) {
+    alert("quizQuestionUL.click");
+    var eventSource = event.target;
+    if (eventSource.matches("button")) {
+        if(stateMachine.quizInitiated == false) {
+            initiateQuiz();
+        } else {
+            var quizNo = eventSource.getAttribute("quiz-index");
+            renderNextQuiz(quizIndex);
+        }
+    }
+});
 
 function pause(ms) {
     var snapshot = new Date().getMilliseconds();
@@ -129,68 +255,5 @@ function pause(ms) {
     }
 }
 
-function renderNextQuiz(index) {
-
-    //Display the separator only when you display the result of first quiz
-    /*if (stateMachine.quizIndex > 0) {
-        resultDiv.style.display = "block";
-    }*/
-    resultDiv.style.display = "none";
-    var quiz = quizQuestionBank[index];
-    var question = quiz.question;
-    //alert("question: " + question);
-    var answerOptions = quiz.answers;
-    answerOptionsUL.textContent = question;
-    for (var i = 0; i < answerOptions.length; i++) {
-        var answerI = answerOptions[i];
-        //alert("answerI: " + answerI);
-        var li = document.createElement("li");
-        var answerButton = document.createElement("button");
-        answerButton.classList.add("btn", "btn-secondary", "btn-sm");
-        answerButton.textContent = answerI;
-
-        answerButton.setAttribute("answer-index", i);
-        //answerButton.setAttribute("quiz-index", index);
-
-        li.appendChild(answerButton);
-        answerOptionsUL.appendChild(li);
-    }
-    //answerOptions.
-}
-
-/*function startQuiz(quizIndex) {
-    var eventSource = event.target;
-    if (eventSource.matches("button")) {
-        var quizNo = eventSource.getAttribute("quiz-index");
-        alert("quizNo: " + quizNo);
-        renderQuiz(quizIndex);
-    }
-}*/
-
-/*renderQuizAnswer(answer) {
-    var li = document.createElement("li");
-    li.textContent = answer;
-    answerOptionsUL.appendChild(li);
-}*/
-
-/* We will listen to the click event on the entire answerOptions<UL> object instead.
-codeQuizForm.addEventListener("submit", function (event) {
-    //event.preventDefault();
-    alert("codeQuizForm form submit");
-    takeQuiz(event);
-});
-
-
-answerOptionsUL.addEventListener("click", function (event) {
-    alert("answerOptionsUL.click");
-    var eventSource = event.target;
-    if (eventSource.matches("button")) {
-        if(stateMachine.quizInitiated == false) {
-            initiateQuiz();
-        } else {
-            var quizNo = eventSource.getAttribute("quiz-index");
-            renderNextQuiz(quizIndex);
-        }
-    }
-});*/
+*/
 
